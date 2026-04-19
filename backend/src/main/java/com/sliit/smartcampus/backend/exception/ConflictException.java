e5b02d75db3be9c93bdd35e99ededd2b9f92cac0
APXs-01 <it23640184@my.sliit.lk>
2026-04-19 20:28:56 +0530

    Add ConflictException for 409 conflict error responses

---
+++ backend/src/main/java/com/sliit/smartcampus/backend/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package com.sliit.smartcampus.backend.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
